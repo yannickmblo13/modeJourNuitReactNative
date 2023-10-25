29eb0e777f397117f8f1266306298778220e0100
Yannick Mblo <yannickmblo2000@gmail.com>
2023-10-25 17:46:17 +0000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [darkMode, setDarkMode] = useState(false);
+
+	function handleToggleMode() {
+		setDarkMode(!darkMode);
+	}
+
+	return (
+		<View style={darkMode ? styles.style1 : styles.style2}>
+			{darkMode ? (
+				<Image
+					style={{ width: 100, height: 100 }}
+					source={require('./assets/sun.png')}
+				/>
+			) : (
+				<Image
+					style={{ width: 100, height: 100 }}
+					source={require('./assets/moon.png')}
+				/>
+			)}
+
+			<Button
+				onPress={handleToggleMode}
+				title={darkMode ? 'Passer au mode Nuit' : 'Passer au mode Jour'}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	style1: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	style2: {
+		flex: 1,
+		backgroundColor: 'black',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
